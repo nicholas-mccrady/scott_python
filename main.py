b76e4e51e5b0d253af562019f7c5b21f4eb38776
nicholas-mccrady <nicholas.mccrady@gmail.com>
2023-01-23 18:21:56 -0600

    second day

---
+++ main.py
@@ -0,0 +1,16 @@
+stmt = "you are a swell dude, yo... so lets add"
+y = "12"
+
+first = input('what is the first number you would like to add?: ')
+second = input('what is the second number you would like to add?: ')
+print(first,second)
+#casting to int and casting to string notice that when are casting to string we are not doing a mathematical operation so it 
+# puts the two variables together instead of adding them or performing and operation on them.##
+#print(str(x) + y)
+# note make sure that you have your formatting in the proper order
+#print (str(x) + '+' + str(y) + '=' + str(int(x) + int(y)))
+#print(stmt)
+#name_user = input("what is your name\n")
+#print ('why is your name ' + str(x) + name_user + "?")
+
+input('press enter')
